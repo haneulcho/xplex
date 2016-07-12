e6dd2c89121664b38f9154738b6c390f77e33de7
Haneul Cho <stuykr@naver.com>
2016-07-12 18:38:48 +0900

    기본 page 마크업 수정

--- wp-content/themes/storevilla/template-parts/content-page.php
+++ wp-content/themes/storevilla/template-parts/content-page.php
@@ -9,21 +9,31 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('storevilla-blog w_view'); ?>>
+	<ul class="blog-meta">
+		<li class="sv-category">출판문화공간 엑스플렉스(X-PLEX)</li>
+	</ul>
 
-	<header class="entry-header">
-		<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
-	</header><!-- .entry-header -->
+	<?php the_title( '<h2>', '</h2>' ); ?>
 
 	<div class="entry-content">
 		<?php
 			the_content();
+
+			if ( function_exists( 'sharing_display' ) ) {
+				sharing_display( '', true );
+			}
+
+			if ( class_exists( 'Jetpack_Likes' ) ) {
+				$custom_likes = new Jetpack_Likes;
+				echo $custom_likes->post_likes( '' );
+			}
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'storevilla' ),
 				'after'  => '</div>',
 			) );
 		?>
-	</div><!-- .entry-content -->	
+	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
